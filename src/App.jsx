36d49e96b7ddf722e5d8a9520e24ae176bc39246
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-16 23:41:06 +0530

    refactor: extract logic to map over books array and render the books to a function renderBooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import { Typography } from "@mui/material"
 function App() {
   const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
 
-  return (
-    <div>
-      <Typography variant="h2" component="h2" data-test="heading">
-        Bookish
-      </Typography>
-      <div data-test="book-list">
+  const renderBooks = (books) => {
+    return <div data-test="book-list">
         {books.map((book, id) => (
           <div key={id} className="book-item">
             <h2 className="title">{book.name}</h2>
           </div>
         ))}
-      </div>
+      </div>;
+  }
+
+  return (
+    <div>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+      {renderBooks(books)}
     </div>
   )
 }
