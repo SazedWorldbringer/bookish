a727bc4f5e7f6f680e7d7bcc752367fd1e89a55e
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-17 00:04:21 +0530

    refactor: fetch books from the stub-server instead of hard coding them

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { Typography } from "@mui/material"
+import axios from "axios";
+import { useState } from "react";
 import BookList from "./BookList";
 
-function App() {
-  const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
+const App = () => {
+  const [books, setBooks] = useState([])
+
+  useState(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get("http://localhost:8080/books");
+      setBooks(res.data);
+    };
+    fetchBooks();
+  }, []);
 
   return (
     <div>
