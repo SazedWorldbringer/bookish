97a81ca35140db1b774c80d07e978b32a08f1b1f
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-16 10:05:00 +0530

    test: install cypress and setup e2e testing, write test to check app loads correctly with Bookish heading, refactor App.jsx to pass the test

---
+++ cypress/e2e/bookish.cy.js
@@ -0,0 +1,6 @@
+describe("Bookish", () => {
+  it("Visits Bookish", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("h2[data-test='heading']").contains("Bookish");
+  })
+})
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material"
 function App() {
   return (
     <div>
-      <Typography variant="h2" component="h2" data-test="heading">Hello world</Typography>
+      <Typography variant="h2" component="h2" data-test="heading">Bookish</Typography>
     </div>
   )
 }
