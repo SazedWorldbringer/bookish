c381fbd19f046fdb33757a8b594096112b5f59e1
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-16 23:28:29 +0530

    test: test for app to render with two books with specific titles

--- cypress/e2e/bookish.cy.js
+++ cypress/e2e/bookish.cy.js
@@ -3,4 +3,15 @@ describe("Bookish", () => {
     cy.visit("http://localhost:5173");
     cy.get("h2[data-test='heading']").contains("Bookish");
   })
+
+  it("Shows a book list", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("div[data-test='book-list']").should("exist");
+    cy.get("div.book-item").should(books => {
+      expect(books).to.have.length(2);
+
+      const titles = [...books].map(x => x.querySelector("h2").innerHTML);
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"])
+    });
+  })
 })
