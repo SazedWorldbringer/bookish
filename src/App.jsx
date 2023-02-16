77ca16cec20a2ce200fb28c42cf5c35fd4a75668
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-16 23:29:11 +0530

    feat: render book list with two books

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import { Typography } from "@mui/material"
 function App() {
   return (
     <div>
-      <Typography variant="h2" component="h2" data-test="heading">Bookish</Typography>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+      <div data-test="book-list">
+        <div className="book-item">
+          <h2>Refactoring</h2>
+        </div>
+        <div className="book-item">
+          <h2>Domain-driven design</h2>
+        </div>
+      </div>
     </div>
   )
 }
