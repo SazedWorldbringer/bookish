3efdf85c2cb22d793cbef0d8b9a95955a351061d
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-16 09:52:44 +0530

    refactor: install mui and use Typography element in App for the heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Typography } from "@mui/material"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <Typography variant="h2" component="h2" data-test="heading">Hello world</Typography>
     </div>
   )
 }
